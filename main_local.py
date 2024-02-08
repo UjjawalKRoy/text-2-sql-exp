59eed622206c213825ca90cc8ebfad8e157c4e7c
UjjawalKRoy <ujjawal.roy@mindinventory.com>
2024-02-08 14:34:31 +0530

    added main_local.py

---
+++ main_local.py
@@ -0,0 +1,78 @@
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import FewShotPromptTemplate
+from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.prompts.prompt import PromptTemplate
+from langchain.utilities.sql_database import SQLDatabase
+from langchain_community.vectorstores import Chroma
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain_community.llms import LlamaCpp
+
+import system_prompt
+from examples import emp_profile_few_shots
+from ques import ques
+
+import langchain
+
+langchain.verbose = True
+
+# api_key = 'AIzaSyA5npGkRSAWoCt4P93ztBzl0o2lIk8GOnI'
+# llm = GoogleGenerativeAI(model="models/text-bison-001", google_api_key=api_key)
+
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+llm = LlamaCpp(
+    model_path="D:/models/NeuralBeagle14-7B.Q8_0.gguf",
+    n_ctx=8000,
+    n_gpu_layers=40,
+    n_threads=8,
+    n_batch=512,
+    max_tokens=200,
+    temperature=0.1,
+    top_p=0.1,
+    callback_manager=callback_manager
+)
+
+
+# print(db.table_info)
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+to_vectorize = [" ".join(example.values()) for example in emp_profile_few_shots]
+vectorstore = Chroma.from_texts(to_vectorize, embeddings, metadatas=emp_profile_few_shots)
+# vectorstore = vectorstore.as_retriever(search_type="mmr", search_kwargs={'k': 1, 'fetch_k': 5, 'lambda_mult': 0.5})
+# docs = vectorstore.get_relevant_documents(query)
+example_selector = SemanticSimilarityExampleSelector(
+    vectorstore=vectorstore,
+    k=1,
+)
+
+example_prompt = PromptTemplate(
+    input_variables=["Question", "SQLQuery", "SQLResult", "Answer", ],
+    template="\nQuestion: {Question}\nSQLQuery: {SQLQuery}\nSQLResult: {SQLResult}\nAnswer: {Answer}",
+)
+
+few_shot_prompt = FewShotPromptTemplate(
+    example_selector=example_selector,
+    example_prompt=example_prompt,
+    prefix=system_prompt.DEFAULT_PROMPT,
+    suffix=PROMPT_SUFFIX,
+    input_variables=["input", "table_info", "top_k"],  # These variables are used in the prefix and suffix
+)
+
+
+def create_db_chain(tables: list[str], query: str):
+    db = SQLDatabase.from_uri("mysql://root:password@localhost/mrms", include_tables=tables)
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True, prompt=few_shot_prompt, return_direct=True)
+    # for q in ques:
+    #     db_chain.invoke({"query": q})
+    qns1 = db_chain.invoke({"query": query})
+    res = qns1["result"]
+    if not res:
+        return None
+    return {"result": res}
+
+
+if __name__ == "__main__":
+    tabs = ['employee', 'leave_application', 'wfh_application', 'employee_occupancy', 'department_type',
+            'issue_work_log',
+            'attendance_event', 'lunch_menu', 'leave_balance']
+    create_db_chain(tables=tabs, query="Who has not filled their logs today?")
